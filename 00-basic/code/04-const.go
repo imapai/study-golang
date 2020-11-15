68eff3ef8122faf3bb83796e2bfccc26ec1cfd63
chengming <15516892602@163.com>
2020-11-16 00:23:19 +0800

    :sparkles:feat(数据结构): 基本的数据类型
    :sparkles:feat(变量声明): 变量的不同形式和定义赋值

---
+++ 00-basic/code/04-const.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	const name string = "zs"
+	const name2 = "ls"
+	const name3, name4 string = "zs", "ls"
+	const name5, age = "zs", 13
+	println(name5, age)
+}
---
+++ 00-basic/code/04-variable.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	var addr string = "zhengzhou"
+	println(addr)
+	var addr2 = "zhengzhou2"
+	println(addr2)
+	addr3 := "zhengzhou3"
+	println(addr3)
+
+	var addr4, addr5 string = "zhengzhou4", "zhengzhou5"
+	println(addr4, addr5)
+	var name, age = "zs", 13
+	println(name, age)
+	name2, age2 := "zs2", 23
+	println(name2, age2)
+}
